892a30c2f7dc34adff2cac0f0591883289105099
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-28 06:51:07 +0700

    Create Yanhh3DProvider.kt

---
+++ Yanhh3DProvider/src/main/java/com/lagradost/Yanhh3DProvider.kt
@@ -0,0 +1,28 @@
+package com.lagradost
+
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.HomePageRequest
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newMovieSearchResponse
+import com.lagradost.cloudstream3.app
+
+class Yanhh3DProvider : MainAPI() {
+    override var mainUrl = "https://yanhh3d.sh"
+    override var name = "Yanhh3D"
+    override val supportedTypes = setOf(TvType.Movie)
+
+    override suspend fun getMainPage(page: Int, request: HomePageRequest): HomePageResponse {
+        val document = app.get(mainUrl).document
+        val home = document.select("article").mapNotNull {
+            val title = it.selectFirst("h2")?.text() ?: return@mapNotNull null
+            val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val poster = it.selectFirst("img")?.attr("src")
+            newMovieSearchResponse(title, href, TvType.Movie) {
+                this.posterUrl = poster
+            }
+        }
+        return newHomePageResponse("Phim Mới", home)
+    }
+}
