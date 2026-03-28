0ecba3d0f5547a253f7959511eb42d60195d5f2e
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-29 06:33:45 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
         maven { url = uri("[https://jitpack.io](https://jitpack.io)") }
     }
     dependencies {
-        // Sử dụng phiên bản ổn định hơn thay vì -SNAPSHOT nếu có thể
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
@@ -17,14 +16,12 @@ apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
 configure<CloudstreamExtension> {
-    // Tự động lấy thông tin từ gradle.properties
     setDisplayName(project.findProperty("moduleName") as String? ?: "Yanhh3D")
     setPackageName(project.findProperty("moduleClassName") as String? ?: "com.yourname.Yanhh3DProvider")
     setDescription(project.findProperty("description") as String? ?: "Cloudstream Provider")
 }
 
 dependencies {
-    // Sử dụng master-SNAPSHOT để luôn cập nhật các tính năng mới nhất từ Cloudstream
     compileOnly("com.github.recloudstream:cloudstream:master-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
 }
