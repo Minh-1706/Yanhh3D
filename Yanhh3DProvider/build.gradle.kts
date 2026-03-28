0ce06b957a5d1f57620e4298a3e5cb6a64849581
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-28 08:21:57 +0700

    Update build.gradle.kts

--- Yanhh3DProvider/build.gradle.kts
+++ Yanhh3DProvider/build.gradle.kts
@@ -1,11 +1,11 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.gradle.api.Project
 
 apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
 configure<CloudstreamExtension> {
-    // Chúng ta dùng hàm cloudstream trực tiếp, đây là cách viết chuẩn của template này
-    cloudstream("Yanhh3D", "com.lagradost", "Yanhh3DProvider")
+    name = "Yanhh3D"
+    pkg = "com.lagradost"
+    mainClass = "Yanhh3DProvider"
 }
