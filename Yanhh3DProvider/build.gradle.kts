9486f7ff188d70b978274d24be0a9c3a5a83fb10
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-28 08:35:49 +0700

    Update build.gradle.kts

--- Yanhh3DProvider/build.gradle.kts
+++ Yanhh3DProvider/build.gradle.kts
@@ -4,7 +4,9 @@ apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-configure<CloudstreamExtension> {
-    // Sử dụng hàm thay vì gán biến để tránh lỗi "Val cannot be reassigned"
-    setProject("Yanhh3D", "com.lagradost", "Yanhh3DProvider")
+cloudstream {
+    name = "Yanhh3D"
+    description = "Yanhh3D Provider"
+    pkg = "com.lagradost"
+    authors = listOf("Minh-1706")
 }
