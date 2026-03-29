496c6e1a04ce76626c3743a77965457967ec3845
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-29 07:03:36 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,15 +15,16 @@ apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-// Cách viết đơn giản hơn để tránh lỗi Unresolved reference
-extensions.configure<CloudstreamExtension> {
+// Sử dụng trực tiếp block cloudstream thay vì extensions.configure
+// Điều này giúp trình biên dịch Gradle nhận diện hàm dễ hơn
+configure<CloudstreamExtension> {
     setDisplayName(project.findProperty("moduleName")?.toString() ?: "Yanhh3D")
     setPackageName(project.findProperty("moduleClassName")?.toString() ?: "com.yourname.Yanhh3DProvider")
     setDescription(project.findProperty("description")?.toString() ?: "Cloudstream Provider")
 }
 
 dependencies {
-    // Luôn dùng bản master-SNAPSHOT để đồng bộ với Plugin
-    compileOnly("com.github.recloudstream:cloudstream:master-SNAPSHOT")
+    val cloudstreamVersion = "master-SNAPSHOT"
+    compileOnly("com.github.recloudstream:cloudstream:$cloudstreamVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
 }
